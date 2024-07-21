1f5ac5569e9ff60da8d9720c2fef42913a3d1e47
Ludovic Gardy <ludovic.gardy@gmail.com>
2024-07-21 21:50:53 +0200

    Subject: Added password verification for sensitive areas
    In this revision, we've enhanced security by introducing a password validation mechanism for certain areas of the app. This ensures that only authorized users can access sensitive information. With the addition of HMAC-based password checking, we've improved data security measures.
    
    In addition, we've introduced a few UI changes. For instance, renaming one of the navigation pages reflects changes in the app's naming convention. We've also restricted team selection to specific contexts to maintain consistent information flow.
    
    These modifications cater to both better user authentication and an improved user interface for app navigation.

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import streamlit as st
 from modules.config import page_config
 from modules.ui_components import init_page_config, load_css, display_sidebar, display_home_tab, display_add_remove_tab, init_session_state
+import hmac
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
 
 class App:
     def __init__(self):
@@ -15,14 +39,14 @@ class App:
             display_sidebar(page_config)
 
         pg = st.navigation([
-            st.Page(self.page1, title="Accueil", icon="🏠"),
-            st.Page(self.page2, title="A propos", icon="ℹ️"),
+            st.Page(self.accueil, title="Accueil", icon="🏠"),
+            st.Page(self.maite, title="MAITE", icon="🌾"),
         ])
         pg.run()
 
-    def page1(self):
+    def accueil(self):
 
-        team_name = st.sidebar.selectbox("Sélectionner une équipe", ["team_build", "team_deploy", "team_test"])
+        team_name = st.sidebar.selectbox("Sélectionner une équipe", ["team_test"])
 
         tabs = st.tabs(["Accueil", "Ajouter/Supprimer un membre"])
 
@@ -32,9 +56,20 @@ class App:
         with tabs[1]:
             display_add_remove_tab(team_name)
 
-    def page2(self):
-        st.write("# A propos")
-        st.write("Cette page n'est pas encore implémentée.")
+    def maite(self):
+
+        if not check_password():
+            st.stop()
+
+        team_name = st.sidebar.selectbox("Sélectionner une équipe", ["team_build", "team_deploy"])
+
+        tabs = st.tabs(["Accueil", "Ajouter/Supprimer un membre"])
+
+        with tabs[0]:
+            display_home_tab(team_name)
+
+        with tabs[1]:
+            display_add_remove_tab(team_name)
 
     
 if __name__ == '__main__':
